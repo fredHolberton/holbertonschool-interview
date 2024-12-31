9a9032031e9325556a5ae227a55d505c446133ac
fredHolberton <frederic.urbani@holbertonstudents.com>
2024-12-31 16:43:05 +0100

    task 0 Fix 1

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -6,24 +6,32 @@ list_boxes = []
 
 def canUnlockAll(boxes):
     """ Method that return true if all boxes can be opened """
+    # Declaration, vidage et remplissage de la liste des booléen
     global list_boxes 
     list_boxes.clear()
     for box in boxes:
-        list_boxes.append(False)     
-    unlockBox(boxes, 0)
+        list_boxes.append(False)
+    # Ouverture de la première box
+    list_boxes[0] = True
+    # Création d'une liste de clés 
+    # et ajout des clés contenues dans la première box
+    list_cles = []
+    for i in boxes[0]:
+        list_cles.append(i)
+    i = 0
+    while i < len(list_cles):
+        index = list_cles[i]
+        if index >= 0 and index < len(boxes):
+            if list_boxes[index] == False:
+                list_boxes[index] = True
+                print("Ouverture de la boite", index)
+                for j in boxes[index]:
+                    list_cles.insert(i + 1, j)
+        i += 1
+        
     return_value = True
     for value in list_boxes:
         if value == False:
             return_value = False
     return return_value
-     
-def unlockBox(boxes, index):
-    """ Method that opens all boxes corresponding to each key
-        contained in the boxes[index] 
-    """
-    if index >= 0 and index < len(boxes):
-        if list_boxes[index] == False:
-            list_boxes[index] = True
-            for i in boxes[index]:     
-                unlockBox(boxes, i)
     
