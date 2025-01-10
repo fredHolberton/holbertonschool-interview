f50c4fbf16de3fee2d1493ae5c7ae3e05f3de0cb
fredHolberton <frederic.urbani@holbertonstudents.com>
2025-01-10 11:14:57 +0100

    Task 0 Fix 1

---
+++ insert_in_sorted_linked_list/lists.h
@@ -0,0 +1,50 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stddef.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct listint_s
+{
+    int n;
+    struct listint_s *next;
+} listint_t;
+
+/**
+ * print_listint - prints all elements of a listint_t list
+ * @h: pointer to head of list
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h);
+
+/**
+ * add_nodeint_end - adds a new node at the end of a listint_t list
+ * @head: pointer to pointer of first node of listint_t list
+ * @n: integer to be included in new node
+ * Return: address of the new element or NULL if it fails
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n);
+
+/**
+ * free_listint - frees a listint_t list
+ * @head: pointer to list to be freed
+ * Return: void
+ */
+void free_listint(listint_t *head);
+
+/**
+ * insert_node - inserts a number into a sorted singly linked list
+ * @head: pointer to list
+ * @number: integer number to insert
+ * Return: address of the new node, or NULL if it failed
+ */
+listint_t *insert_node(listint_t **head, int number);
+
+#endif /* LISTS_H */
