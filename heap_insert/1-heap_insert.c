d01ded30216e4ba3dccd63120218cfa228bc57c5
fredHolberton <frederic.urbani@holbertonstudents.com>
2025-01-17 22:53:27 +0100

    task 1 Fix 5

--- heap_insert/1-heap_insert.c
+++ heap_insert/1-heap_insert.c
@@ -4,29 +4,29 @@
 
 /**
  * swap_new_node - Restores the Max Heap property by swapping nodes upward
- * @node: Pointer to the node to swap up 
+ * @node: Pointer to the node to swap up
  *
  * Return: Pointer to the node after swapping
  */
 heap_t *swap_new_node(heap_t *node)
 {
-        heap_t *parent;
-        int temp;
+	heap_t *parent;
+	int temp;
 
-        while (node->parent && node->n > node->parent->n)
-        {
-                parent = node->parent;
+	while (node->parent && node->n > node->parent->n)
+	{
+		parent = node->parent;
 
-                /* Swap values */
-                temp = node->n;
-                node->n = parent->n;
-                parent->n = temp;
+		/* Swap values */
+		temp = node->n;
+		node->n = parent->n;
+		parent->n = temp;
 
-                /* Move up */
-                node = parent;
-        }
+		/* Move up */
+		node = parent;
+	}
 
-        return (node);
+	return (node);
 }
 
 /**
@@ -41,22 +41,22 @@ heap_t *find_insertion_parent(heap_t *root)
 	int currentIndex = 0;
 	int maxIndex = 0;
 
-        file[maxIndex++] = root;
+	file[maxIndex++] = root;
 
-        while (currentIndex < maxIndex)
-        {
-                heap_t *current = file[currentIndex++];
+	while (currentIndex < maxIndex)
+	{
+		heap_t *current = file[currentIndex++];
 
-                /* If current node has one NULL child -> return node */
-                if (!current->left || !current->right)
-                        return (current);
+		/* If current node has one NULL child -> return node */
+		if (!current->left || !current->right)
+			return (current);
 
-                /* else Add children to the file */
-                file[maxIndex++] = current->left;
-                file[maxIndex++] = current->right;
-        }
+		/* else Add children to the file */
+		file[maxIndex++] = current->left;
+		file[maxIndex++] = current->right;
+	}
 
-        return (NULL);
+	return (NULL);
 }
 
 /**
@@ -86,17 +86,17 @@ heap_t *heap_insert(heap_t **root, int value)
 		return (*root);
 	}
 
-	// Find the parent node to insert child
+	/* Find the parent node to insert child */
 	parent = find_insertion_parent(*root);
 
-	// add a new node to the parent founded
+	/* add a new node to the parent founded */
 	newNode->parent = parent;
 	if (!parent->left)
 		parent->left = newNode;
 	else
 		parent->right = newNode;
 
-	// while newNode is not to the right place, swap with parent
+	/* while newNode is not to the right place, swap with parent */
 	return (swap_new_node(newNode));
 }
 
