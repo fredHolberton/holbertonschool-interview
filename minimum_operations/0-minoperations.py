e2db35d475ee9fd737f060e73a074b069b743efb
fredHolberton <frederic.urbani@holbertonstudents.com>
2025-01-21 15:00:09 +0100

    Task 0 Fix 2

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -20,15 +20,17 @@ def greatestDivisor(n):
     return m
 
 def PowerOf(n):
-    m = n // 2
-    p = 2
+    i = int(n/4)
+    p = i
     isPowerOf = False
-    while (p < n) and (isPowerOf is False):
-        while (m > 2) and (isPowerOf is False):
+    while (p < i) and (isPowerOf is False):
+        m = 2
+        while (m < i) and (isPowerOf is False):
+            print("{} ^ {}".format(m,p))
             if (m ** p) == n:
                 isPowerOf = True
             else:
-                m = m - 1
+                m = m + 1
         if isPowerOf is False:
             p = p + 1
     if isPowerOf is True:
@@ -42,14 +44,17 @@ def minOperations(n):
     if n <= 5:
         return n
     if isPrimeNumber (n):
+        print("{} est un nombre premier".format(n))
         return n
     p = PowerOf(n)
     if p != 0:
         m = int(n ** (1/p))
+        print("{} = {} ^ {}".format(n, m, p))
         return minOperations(m) + p + 1
     else:
         m = greatestDivisor(n)
         d = n // m
+        print("{} = {} x {}".format(n, m, d))
         return minOperations(m) + d
     
     
