8a8a37f875fab8b5c64b9ce87173666333778127
fredHolberton <frederic.urbani@holbertonstudents.com>
2025-01-30 16:46:34 +0100

    task 0 Fix 1

---
+++ sandpiles/sandpiles.h
@@ -0,0 +1,15 @@
+#ifndef SANDPILES_H
+#define SANDPILES_H
+
+#include <stddef.h>
+
+/**
+ * sandpiles_sum - Computes the sum of two sandpiles
+ * @grid1: Left 3x3 grid
+ * @grid2: Right 3x3 grid
+ *
+ */
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+
+#endif /* SANDPILES_H */
+
