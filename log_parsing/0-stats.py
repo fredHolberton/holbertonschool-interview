cbd226d52235b327167c7a29a34d4bcf4b89e255
fredHolberton <frederic.urbani@holbertonstudents.com>
2025-02-10 17:27:54 +0100

    Task 0 Fix 4

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -19,30 +19,36 @@ status_codes = {
 }
 file_size = 0
 
-while True:
-    for i in range(10):
-        line = sys.stdin.readline()
-        line = line.strip()
+try:        
+    while True:
+        for i in range(10):
+            line = sys.stdin.readline()
+            line = line.strip()
 
-        avant, separateur, apres = line.partition(' - [')
-        if re.match(reg_IP, avant):
-            # une adresse ip est trouvée, on continue
-            end_line = apres
-            avant, separateur, apres = end_line.partition('] "')
-            if re.match(datetime_regex, avant):
-                # une date est trouvée, on continue
+            avant, separateur, apres = line.partition(' - [')
+            if re.match(reg_IP, avant):
+                # une adresse ip est trouvée, on continue
                 end_line = apres
-                avant, separateur, apres = end_line.partition('" ')
-                if avant == 'GET /projects/260 HTTP/1.1': 
-                    # la chaine "GET /projects/260 HTTP/1.1" est trouvée
+                avant, separateur, apres = end_line.partition('] "')
+                if re.match(datetime_regex, avant):
+                    # une date est trouvée, on continue
                     end_line = apres
-                    avant, separateur, apres = end_line.partition(' ')
-                    if (avant in status_codes) and apres.isdigit():
-                        status_codes[avant] += 1
-                        file_size += int(apres)
-                        
+                    avant, separateur, apres = end_line.partition('" ')
+                    if avant == 'GET /projects/260 HTTP/1.1': 
+                        # la chaine "GET /projects/260 HTTP/1.1" est trouvée
+                        end_line = apres
+                        avant, separateur, apres = end_line.partition(' ')
+                        if (avant in status_codes) and apres.isdigit():
+                            status_codes[avant] += 1
+                            file_size += int(apres)
+                            
+        print(f"File size: {file_size}")
+        for cle, valeur in status_codes.items():
+            if valeur != 0:
+                print(f"{cle}: {valeur}")
+except KeyboardInterrupt:
     print(f"File size: {file_size}")
     for cle, valeur in status_codes.items():
         if valeur != 0:
             print(f"{cle}: {valeur}")
-    
+
