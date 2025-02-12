64e206458dff2342aad98edb19e3a7486cd1e298
fredHolberton <frederic.urbani@holbertonstudents.com>
2025-02-12 09:46:57 +0100

    Task 0 Fix 11

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -23,50 +23,50 @@ line_count = 0
 
 try:        
     while True:  
-        #rlist, _, _ = select.select([sys.stdin], [], [], 0.1)
-        #if rlist:
+        rlist, _, _ = select.select([sys.stdin], [], [], 0.1)
+        if rlist:
             # il y a encore des lignes dans stdin                   
-        line = sys.stdin.read().strip()
+            line = sys.stdin.read().strip()
             
-        if not line:
-            # plus d'infos dans stdin
+            if not line:
+                # plus d'infos dans stdin
+                print(f"File size: {file_size}")
+                for cle, valeur in status_codes.items():
+                    if valeur != 0:
+                        print(f"{cle}: {valeur}")   
+                sys.exit(0)
+                 
+            line_count += 1
+            avant, separateur, apres = line.partition(' - [')
+            if re.match(reg_IP, avant):
+                # une adresse ip est trouvée, on continue
+                end_line = apres
+                avant, separateur, apres = end_line.partition('] "')
+                if re.match(datetime_regex, avant):
+                    # une date est trouvée, on continue
+                    end_line = apres
+                    avant, separateur, apres = end_line.partition('" ')
+                    if avant == 'GET /projects/260 HTTP/1.1': 
+                        # la chaine "GET /projects/260 HTTP/1.1" est trouvée
+                        end_line = apres
+                        avant, separateur, apres = end_line.partition(' ')
+                        if (avant in status_codes) and apres.isdigit():
+                            status_codes[avant] += 1
+                            file_size += int(apres)
+                            
+                            if line_count % 10 == 0:
+                                print(f"File size: {file_size}")
+                                for cle, valeur in status_codes.items():
+                                    if valeur != 0:
+                                        print(f"{cle}: {valeur}") 
+        
+        else:
             print(f"File size: {file_size}")
             for cle, valeur in status_codes.items():
                 if valeur != 0:
-                    print(f"{cle}: {valeur}")   
-            sys.exit(0)
-                 
-        line_count += 1
-        avant, separateur, apres = line.partition(' - [')
-        if re.match(reg_IP, avant):
-            # une adresse ip est trouvée, on continue
-            end_line = apres
-            avant, separateur, apres = end_line.partition('] "')
-            if re.match(datetime_regex, avant):
-                # une date est trouvée, on continue
-                end_line = apres
-                avant, separateur, apres = end_line.partition('" ')
-                if avant == 'GET /projects/260 HTTP/1.1': 
-                    # la chaine "GET /projects/260 HTTP/1.1" est trouvée
-                    end_line = apres
-                    avant, separateur, apres = end_line.partition(' ')
-                    if (avant in status_codes) and apres.isdigit():
-                        status_codes[avant] += 1
-                        file_size += int(apres)
-                            
-                        if line_count % 10 == 0:
-                            print(f"File size: {file_size}")
-                            for cle, valeur in status_codes.items():
-                                if valeur != 0:
-                                    print(f"{cle}: {valeur}") 
-        
-        #else:
-        #    print(f"File size: {file_size}")
-        #    for cle, valeur in status_codes.items():
-        #        if valeur != 0:
-        #           print(f"{cle}: {valeur}")
-        #    if line_count == 0:
-        #        sys.exit(0)        
+                    print(f"{cle}: {valeur}")
+            if line_count == 0:
+                sys.exit(0)        
                
 except KeyboardInterrupt:
     print(f"File size: {file_size}")
