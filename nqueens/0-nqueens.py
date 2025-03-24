1e7082148df160280467f78c6b8e9e722c3f9402
fredHolberton <frederic.urbani@holbertonstudents.com>
2025-03-25 00:05:27 +0100

    Task 0 Fix 1

--- nqueens/0-nqueens.py
+++ nqueens/0-nqueens.py
@@ -4,11 +4,15 @@
 
 import sys
 
-if sys.argv.count != 2:
-    print("Usage: nqueens N\n")
+nbArg = len(sys.argv)
+if nbArg != 2:
+    print("Usage: nqueens N")
     sys.exit(1)
     
 a = int(sys.argv[1])
 if a < 4:
-    print("N must be at least 4\n")
+    print("N must be at least 4")
     sys.exit(1)
+    
+print("Command OK")
+sys.exit
