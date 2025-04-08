8b7d3908d27dcb9ecef60504dc957ceea13bb22a
fredHolberton <frederic.urbani@holbertonstudents.com>
2025-04-08 14:50:48 +0200

    0. Linear search in a skip list

---
+++ linear_skip/0-linear_skip.c
@@ -0,0 +1,45 @@
+#include "search.h"
+#include <math.h>
+
+/**
+ * linear_skip - Searches for a value in a sorted skip list of integers.
+ * @list: Pointer to the head of the skip list to search in
+ * @value: Value to search for
+ * Return: Pointer on the first node where value is located
+ */
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+	skiplist_t *node = list;
+	skiplist_t *express_node = NULL;
+
+	if (!list)
+		return (NULL);
+
+	/* Traverse the skip list using the express lane */
+	while (node && node->express)
+	{
+		express_node = node->express;
+		/* Print the current node value */
+		printf("Value checked at index [%lu] = [%d]\n", node->index, node->n);
+		if (express_node->n >= value)
+		{
+			break;
+		}
+		node = express_node;
+	}
+	/* Print the range we're searching between */
+	printf("Value found between indexes [%lu] and [%lu]\n"\
+	, node->index, express_node->index);
+	/* Perform linear search within the range between node and express_node */
+	while (node)
+	{
+		printf("Value checked at index [%lu] = [%d]\n", node->index, node->n);
+		if (node->n == value)
+			return (node);
+		if (node == express_node)
+			break;
+		node = node->next;
+	}
+	return (NULL);
+}
+
---
+++ linear_skip/search.h
@@ -0,0 +1,42 @@
+#ifndef _SEARCH_H_
+#define _SEARCH_H_
+
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+
+/**
+ * struct skiplist_s - Singly linked list with an express lane
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ * @express: Pointer to the next node in the express lane
+ *
+ * Description: singly linked list node structure with an express lane
+ * for Holberton project
+ */
+typedef struct skiplist_s
+{
+	int n;
+	size_t index;
+	struct skiplist_s *next;
+	struct skiplist_s *express;
+} skiplist_t;
+
+skiplist_t *create_skiplist(int *array, size_t size);
+void print_skiplist(const skiplist_t *list);
+void free_skiplist(skiplist_t *list);
+skiplist_t *linear_skip(skiplist_t *head, int value);
+
+/**
+ * linear_skip - Searches for a value in a sorted skip list of integers.
+ * @list: Pointer to the head of the skip list to search in
+ * @value: Value to search for
+ * Return: Pointer on the first node where value is located
+ */
+skiplist_t *linear_skip(skiplist_t *list, int value);
+
+#endif /* _SEARCH_H_ */
