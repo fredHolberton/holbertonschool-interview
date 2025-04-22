9312d901bb3b3f475dfab7b8cb235bcf7c0f4841
fredHolberton <10196@holbertonstudents.com>
2025-04-22 14:42:52 +0200

    0. Rain

---
+++ rain/0-rain.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" Module : Rain """
+
+
+import sys
+
+
+def rain(walls):
+    """Return: Integer indicating total amount of rainwater retained."""
+    if not walls:
+        return 0
+
+    left, right = 0, len(walls) - 1
+    left_max, right_max = 0, 0
+    water = 0
+
+    while left < right:
+        if walls[left] < walls[right]:
+            if walls[left] >= left_max:
+                left_max = walls[left]
+            else:
+                water += left_max - walls[left]
+            left += 1
+        else:
+            if walls[right] >= right_max:
+                right_max = walls[right]
+            else:
+                water += right_max - walls[right]
+            right -= 1
+
+    return water
+
---
+++ rain/README.md
@@ -0,0 +1,1 @@
+Contains file of project: Rain
