6001b0c9eea93d66df6e1fe7d38e52a86e76cd01
fredHolberton <10196@holbertonstudents.com>
2025-07-01 09:23:36 +0200

    0. Add Nodes: Fix betty coding style2

--- double_circular_linked_list/0-add_node.c
+++ double_circular_linked_list/0-add_node.c
@@ -46,7 +46,7 @@ List *add_node_end(List **list, char *str)
 }
 
 /**
- * add_node_begin - Adds a new node to the beginning of a double circular linked list
+ * add_node_begin - Adds a new node to the beginning of the linked list
  * @list: Pointer to the pointer of the list to modify
  * @str: String to copy into the new node
  *
