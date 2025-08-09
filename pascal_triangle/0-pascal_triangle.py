62bd2e31b37560cd16a0f259976d0fbea0c08916
fredHolberton <10196@holbertonstudents.com>
2025-08-09 16:42:10 +0200

    0. Pascal s Triangle: add comment

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
+""" Module : pascal_triangle """
+
 def pascal_triangle(n):
-    """
-    Returns a list of lists of integers representing Pascal's triangle of n.
-    Returns an empty list if n <= 0
-    """
+    """ Return a list of lists of integer like Pascal's triangle of n """
+
     if n <= 0:
         return []
 
